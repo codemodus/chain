93d287faec97845dd5947f8b03288799d537d8d1
Daved <daved@codemodus.com>
2015-05-18 00:09:46 -0700

    Added comments and cleaned up a couple of minor things.

--- chain.go
+++ chain.go
@@ -1,3 +1,6 @@
+// Package chain enables flexible reordering and reuse of nested functions,
+// Some convenience functions are also provided for easing the passing of data
+// through instances of Chain.
 package chain
 
 import (
@@ -6,16 +9,22 @@ import (
 	"golang.org/x/net/context"
 )
 
+// Handler interface must be implemented for an object to be included within
+// a Chain.
 type Handler interface {
 	ServeHTTPContext(context.Context, http.ResponseWriter, *http.Request)
 }
 
+// HandlerFunc is an adapter which allows functions with the appropriate
+// signature to be, subsequently, treated as a Handler.
 type HandlerFunc func(context.Context, http.ResponseWriter, *http.Request)
 
+// ServeHTTPContext calls h(c, w, r)
 func (h HandlerFunc) ServeHTTPContext(c context.Context, w http.ResponseWriter, r *http.Request) {
 	h(c, w, r)
 }
 
+// Chain holds the basic components used to order handler wraps.
 type Chain struct {
 	ctx context.Context
 	m   []func(Handler) Handler
@@ -32,18 +41,22 @@ func (ha handlerAdapter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 type noCtxHandlerAdapter struct {
 	handlerAdapter
-	mw  func(http.Handler) http.Handler
+	mw func(http.Handler) http.Handler
 }
 
+// New takes one or more Handler wraps, and returns a new Chain.
 func New(ctx context.Context, mw ...func(Handler) Handler) Chain {
 	return Chain{ctx: ctx, m: mw}
 }
 
+// Append takes one or more Handler wraps, and appends it/them to the returned
+// Chain.
 func (c Chain) Append(mw ...func(Handler) Handler) Chain {
 	c.m = append(c.m, mw...)
 	return c
 }
 
+// End takes a Handler and returns an http.Handler.
 func (c Chain) End(h Handler) http.Handler {
 	if h == nil {
 		return nil
@@ -59,14 +72,18 @@ func (c Chain) End(h Handler) http.Handler {
 	return f
 }
 
+// EndFn takes a func that matches the HandlerFunc type, assigns it as such if
+// it is not already so, then passes it to End.
 func (c Chain) EndFn(h HandlerFunc) http.Handler {
 	if h == nil {
 		return c.End(nil)
 	}
-	return c.End(HandlerFunc(h))
+	return c.End(h)
 }
 
-// Adapt http.Handler into a ContextHandler
+// Bridge takes a standard http.Handler wrapping function and returns a
+// chain.Handler wrap.  This is useful for making non-context aware
+// http.Handler wraps compatible with the rest of a Chain.
 func Bridge(h func(http.Handler) http.Handler) func(Handler) Handler {
 	return func(n Handler) Handler {
 		return HandlerFunc(
@@ -86,10 +103,15 @@ const (
 	postHandlerFuncCtxKey ctxKey = 0
 )
 
+// InitPHFC takes a context.Context and places a pointer to it within itself.
+// This is useful for carrying data into the post ServeHTTPContext area of
+// Handler wraps.  PHFC stands for Post HandlerFunc Context.
 func InitPHFC(ctx context.Context) context.Context {
 	return context.WithValue(ctx, postHandlerFuncCtxKey, &ctx)
 }
 
+// GetPHFC takes a context.Context and returns a pointer to the context.Context
+// set in InitPHFC.
 func GetPHFC(ctx context.Context) (*context.Context, bool) {
 	cx, ok := ctx.Value(postHandlerFuncCtxKey).(*context.Context)
 	return cx, ok
