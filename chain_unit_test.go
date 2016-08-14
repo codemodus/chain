2d6a4d41df8f7e8644fde7ce4cb7f04d8799681a
Daved <daved@codemodus.com>
2016-08-14 11:36:16 -0700

    Move example into separate file.

--- chain_unit_test.go
+++ chain_unit_test.go
@@ -1,8 +1,6 @@
 package chain
 
 import (
-	"fmt"
-	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -13,50 +11,6 @@ var (
 	b1   = "1"
 	bEnd = "_END_"
 )
-
-func Example() {
-	// Nested handlers write either "0" or "1" to the response body before
-	// and after ServeHTTP() is called.
-	//
-	// endHandler writes "_END_" to the response body.
-
-	ch00 := New(nestedHandler0, nestedHandler0)
-	ch001 := ch00.Append(nestedHandler1)
-
-	ch1 := New(nestedHandler1)
-	ch1001 := ch1.Merge(ch001)
-
-	mux := http.NewServeMux()
-	mux.Handle("/00_End", ch00.EndFn(endHandler))
-	mux.Handle("/001_End", ch001.EndFn(endHandler))
-	mux.Handle("/1001_End", ch1001.EndFn(endHandler))
-
-	server := httptest.NewServer(mux)
-
-	resp00, err := respBody(server.URL + "/00_End")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	resp001, err := respBody(server.URL + "/001_End")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	resp1001, err := respBody(server.URL + "/1001_End")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	fmt.Println("Chain 00 Resp:", resp00)
-	fmt.Println("Chain 001 Resp:", resp001)
-	fmt.Println("Chain 1001 Resp:", resp1001)
-
-	// Output:
-	// Chain 00 Resp: 00_END_00
-	// Chain 001 Resp: 001_END_100
-	// Chain 1001 Resp: 1001_END_1001
-}
 
 func TestUnitNew(t *testing.T) {
 	c := New(emptyNestedHandler)
@@ -145,33 +99,6 @@ func TestUnitEndFnNilHandler(t *testing.T) {
 	if http.StatusOK != w.Code {
 		t.Fatalf("want status %d, got %d\n", http.StatusOK, w.Code)
 	}
-}
-
-func respBody(url string) (string, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return "", err
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-
-	_ = resp.Body.Close()
-
-	return string(body), nil
-}
-
-func respStatus(url string) (int, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return 0, err
-	}
-
-	_ = resp.Body.Close()
-
-	return resp.StatusCode, nil
 }
 
 func record(h http.Handler) (*httptest.ResponseRecorder, error) {
